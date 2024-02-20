966b440b6de61f6e99982afe5b43fbefd3c5441e
romanfomindev <roman.fomindev@onlinesup.com>
2024-02-20 12:08:18 +0300

    chore: removed the check for NA when grouping

--- series/series.go
+++ series/series.go
@@ -123,11 +123,11 @@ const (
 // Indexes represent the elements that can be used for selecting a subset of
 // elements within a Series. Currently supported are:
 //
-//     int            // Matches the given index number
-//     []int          // Matches all given index numbers
-//     []bool         // Matches all elements in a Series marked as true
-//     Series [Int]   // Same as []int
-//     Series [Bool]  // Same as []bool
+//	int            // Matches the given index number
+//	[]int          // Matches all given index numbers
+//	[]bool         // Matches all elements in a Series marked as true
+//	Series [Int]   // Same as []int
+//	Series [Bool]  // Same as []bool
 type Indexes interface{}
 
 // New is the generic Series constructor
